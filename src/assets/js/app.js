937d94ec714b96d7b7c3bd40cc843035d1845978
Brendan Norris <notbrendantanner@gmail.com>
2016-09-29 16:15:29 -0400

    restructured data grabbing

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,12 +11,12 @@
         6.	Self-employment Income
     */
 
-var employment_Income = document.getElementById("employmentIncome");
-var nonemployment_Income = document.getElementById("nonemploymentIncome");
-var investment_Income = document.getElementById("investmentIncome");
-var rent_Income = document.getElementById("rentIncome");
-var hobby_Income = document.getElementById("hobbyIncome");
-var selfemployment_Income = document.getElementById("selfemploymentIncome");
+// var employment_Income = document.getElementById("employmentIncome");
+// var nonemployment_Income = document.getElementById("nonemploymentIncome");
+// var investment_Income = document.getElementById("investmentIncome");
+// var rent_Income = document.getElementById("rentIncome");
+// var hobby_Income = document.getElementById("hobbyIncome");
+// var selfemployment_Income = document.getElementById("selfemploymentIncome");
 
 var income = {};
 
@@ -31,20 +31,111 @@ var income = {};
         7.	Cash
     */
 
-var bank_Accounts = document.getElementById('bankAccounts');
-var investment_Accounts = document.getElementById('investmentAccounts');
-var annuities = document.getElementById('annuities');
-var cash_Equivalent_Policies = document.getElementById('cashEquivalentPolicies');
-var monies_Owed = document.getElementById('moniesOwed');
-var cash = document.getElementById('cash');
+// var bank_Accounts = document.getElementById('bankAccounts');
+// var investment_Accounts = document.getElementById('investmentAccounts');
+// var annuities = document.getElementById('annuities');
+// var cash_Equivalent_Policies = document.getElementById('cashEquivalentPolicies');
+// var monies_Owed = document.getElementById('moniesOwed');
+// var cash = document.getElementById('cash');
 
-var liquid_Assets = {};
+
+// User Basic Info
+var firstName = document.getElementById('firstName').value;
+var lastName = document.getElementById('lastName').value;
+var age = document.getElementById('age').value;
+var gender = document.getElementById('gender').value;
+var address = document.getElementById('address').value;
+var city = document.getElementById('city').value;
+var state = document.getElementById('state').value;
+var zip = document.getElementById('zip').value;
+
+var user = {
+  firstName: firstName,
+  lastName: lastName,
+  age: age,
+  gender: gender,
+  address: address,
+  city: city,
+  state: state,
+  zip: zip
+};
+
+// Income Info
+var incomeType = document.getElementById('incomeType');
+var incomeTotal = document.getElementById('incomeTotal');
+
+var income = {};
+
+// Expense Info
+var expenses = document.getElementById('expenses');
+
+// Debt Info
+var debt = document.getElementById('debt');
+
+// Asset Info
+
+var liquidAssets = {};
+var nonLiquidAssets = {};
+var assets = {};
+
+// Family Info
+var family = {};
+
+// Store data.
+function storeInputData() {
+  // User Basic Info
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+  var age = document.getElementById('age').value;
+  var gender = document.getElementById('gender').value;
+  var address = document.getElementById('address').value;
+  var city = document.getElementById('city').value;
+  var state = document.getElementById('state').value;
+  var zip = document.getElementById('zip').value;
+
+  var user = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+    gender: gender,
+    address: address,
+    city: city,
+    state: state,
+    zip: zip
+  };
+
+  // Income Info
+  var incomeType = document.getElementById('incomeType').value;
+  var incomeTotal = document.getElementById('incomeTotal').value;
+
+  var income = {};
+
+  // Expense Info
+  var expenses = document.getElementById('expenses').value;
+
+  // Debt Info
+  var debt = document.getElementById('debt').value;
+
+  // Asset Info
+
+  var liquidAssets = {};
+  var nonLiquidAssets = {};
+  var assets = {};
+
+  // Family Info
+  var family = {};
+};
 
 //When window has loaded... execute this...
 window.onload = function() {
   $(document).foundation();
   init();
 };
+
+// Update when user changes input data.
+document.getElementById('submit').addEventListener('click', function() {
+  storeInputData();
+}, false);
 
 //GLOBAL NAMESPACE
 
