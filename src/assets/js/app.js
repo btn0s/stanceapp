71ddc25f44a85ebbd601f567984dcd7cf11b5448
Brendan Norris <notbrendantanner@gmail.com>
2016-12-20 20:50:50 -0500

    updated to current js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,147 +1,56 @@
-//declare global variables
 
+app = {
 
-    /*
-    Income:
-        1.	Employment Income
-        2.	Non-employment Income
-        3.	Investment Income
-        4.	Rent Income
-        5.	Hobby Income
-        6.	Self-employment Income
-    */
+    data : {},
 
-// var employment_Income = $("employmentIncome");
-// var nonemployment_Income = $("nonemploymentIncome");
-// var investment_Income = $("investmentIncome");
-// var rent_Income = $("rentIncome");
-// var hobby_Income = $("hobbyIncome");
-// var selfemployment_Income = $("selfemploymentIncome");
+    /**
+     * Loops over all inputs and saves them to the data variable
+     * Inputs must be child to a container with the class {name}-info
+     */
+    saveData : function() {
+        var self = this;
+        $('[class*="-info"]').each(function(ind, val) {
+            // We can also decide section with data-section attribute instead of class if it becomes an issue
+            var section = val.className.match(/[\S]{1,}-info/);
+            var class_name = '.{0}'.format(section);
+            var section_info = {};
+            $('input, select, textarea', $(class_name)).each(function() {
+                var ident = $(this).attr('id');
+                section_info[ident] = $(this).val();
+            });
+            self.data[section] = section_info;
+        });
 
-var income = {};
+        console.log(self.data);
+    },
 
-    /*
-    Liquid Assets:
-        1.	Bank Accounts
-        2.	Investment Accounts (non-retirement)
-        3.	investment Accounts (retirement)
-        4.	Annuities
-        5.	Cash-equivalent policies
-        6.	Monies Owed
-        7.	Cash
-    */
+    bindActions : function() {
+        var self = this;
+        $('input, select, textarea').on('blur', function() {
+            self.saveData();
+        });
 
-// var bank_Accounts = $('bankAccounts');
-// var investment_Accounts = $('investmentAccounts');
-// var annuities = $('annuities');
-// var cash_Equivalent_Policies = $('cashEquivalentPolicies');
-// var monies_Owed = $('moniesOwed');
-// var cash = $('cash');
-
-
-// User Basic Info
-var firstName;
-var lastName;
-var age;
-var gender;
-var address;
-var city;
-var state;
-var zip;
-
-var user = {};
-
-// Income Info
-var incomeType;
-var incomeTotal;
-
-var income = {};
-
-// Expense Info
-var expenses;
-
-// Debt Info
-var debt;
-
-// Asset Info
-
-var liquidAssets = {};
-var nonLiquidAssets = {};
-var assets = {};
-
-// Family Info
-var family = {};
-
-// Store data.
-function storeInputData() {
-  // User Basic Info
-  var firstName = $('#firstName').val();
-  var lastName = $('#lastName').val();
-  var age = $('#age').val();
-  var gender = $('#gender').val();
-  var address = $('#address').val();
-  var city = $('#city').val();
-  var state = $('#state').val();
-  var zip = $('#zip').val();
-
-  var user = {
-    firstName: firstName,
-    lastName: lastName,
-    age: age,
-    gender: gender,
-    address: address,
-    city: city,
-    state: state,
-    zip: zip
-  };
-
-  // Income Info
-  var incomeType = $('#incomeType').val();
-  var incomeTotal = $('#incomeTotal').val();
-
-  var income = {};
-
-  // Expense Info
-  var expenses = $('#expenses').val();
-
-  // Debt Info
-  var debt = $('#debt').val();
-
-  // Asset Info
-
-  var liquidAssets = {};
-  var nonLiquidAssets = {};
-  var assets = {};
-
-  // Family Info
-  var family = {};
-};
-
-//When window has loaded... execute this...
-window.onload = function() {
-  $(document).foundation();
-  init();
+        // No submit button yet, but dropped this here for future use
+        $('#submit').on('click', function() {
+            self.saveData();
+        });
+    }
 };
 
 $(document).ready(function() {
-  // Update when user changes input data.
-  $('#submit').click(function() {
-    storeInputData();
-  });
-
-  // Update when user makes changes.
-  $('input').on( 'blur', function() {
-    storeInputData();
-
-    console.log(firstName);
-  });
+    app.bindActions();
 });
 
-//GLOBAL NAMESPACE
 
-var STANCEAPP = STANCEAPP || {};
-
-STANCEAPPAPP.Event = {};
-
-STANCEAPP.commonmethod = {
-};
+// Function for string formatting. Use like
+// 'This is a {0} string that can be {1} {2}.'.format('Cool', 'formatted')
+// Output >> This is a Cool string that can be formatted {2}.
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number] : match;
+        });
+    };
+}
