cab3658912a812cf090b4265d6aa199c7a79234d
drew4u <drewsibert@gmail.com>
2017-01-05 00:18:16 -0500

    Dashboards semi-complete.
    Still need to style and make sure all IDs are linked to variable in the
    javascript. Still to need to finish side nav as well. You can delete
    chart.bundle.min.js, I ended up using a CDN

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,4 @@
-
+var app;
 app = {
 
     data : {},
@@ -53,16 +53,4 @@ if (!String.prototype.format) {
                 ? args[number] : match;
         });
     };
-}
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
 }
