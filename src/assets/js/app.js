653c201a0969ab118ccdc2ecd160074498582b9f
Brendan Norris <notbrendantanner@gmail.com>
2017-01-06 01:33:54 -0500

    merging old and new

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -103,3 +103,15 @@ if (!String.prototype.format) {
         });
     };
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
